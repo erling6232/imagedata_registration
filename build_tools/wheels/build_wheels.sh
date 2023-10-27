984690f453e6f360d9c1ec05c72c68e1b3f174f0
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2023-10-27 09:27:19 +0200

    Improving build actions.

--- build_tools/wheels/build_wheels.sh
+++ build_tools/wheels/build_wheels.sh
@@ -8,7 +8,9 @@ echo "build_wheels.sh: running"
 # in the pyproject.toml file, while the tests are run
 # against the most recent version of the dependencies
 
-python --version
-env
+pythonversion=$(python --version | awk -e '{print $2}' | awk -F. -e '{print $1 $2}')
+if [[ "${pythonversion}" == "38" ]]; then
+  python -m pip install SimpleITK-SimpleElastix==2.0.0rc2.dev909
+fi
 python -m pip install cibuildwheel
 python -m cibuildwheel --output-dir wheelhouse
