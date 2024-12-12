053deffa57872be0498f4219a52986d79b20fdaa
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2024-12-12 10:16:24 +0100

    Install Rust and Cargo in order to install pylibjpeg-rle.

--- build_tools/wheels/build_wheels.sh
+++ build_tools/wheels/build_wheels.sh
@@ -3,17 +3,10 @@
 set -e
 set -x
 echo "build_wheels.sh: running"
-echo "PATH $PATH"
-which cargo
-cargo --version
 
 # The version of the built dependencies are specified
 # in the pyproject.toml file, while the tests are run
 # against the most recent version of the dependencies
 
-pythonversion=$(python --version | awk -e '{print $2}' | awk -F. -e '{print $1 $2}')
-if [[ "${pythonversion}" == "38" ]]; then
-  python -m pip install SimpleITK-SimpleElastix==2.0.0rc2.dev909
-fi
 python -m pip install cibuildwheel
 python -m cibuildwheel --output-dir wheelhouse
