2138a0649657aa455d623837e4effd1daa6197c6
Erling Andersen <Erling.Andersen@Helse-Bergen.NO>
2026-09-01 08:52:01 +0200

    Let test_topup run on github

--- tests/test_fsl.py
+++ tests/test_fsl.py
@@ -15,6 +15,7 @@ class TestFSLRegistration(unittest.TestCase):
         os.environ['FSLOUTPUTTYPE'] = 'NIFTI_GZ'
 
     def test_register_fsl(self):
+        print('test_register_fsl:')
         # if os.getenv("GITHUB_ACTION") is not None:
         #     return
         a = Series('data/time.zip', 'time')
@@ -47,8 +48,9 @@ class TestFSLDWI(unittest.TestCase):
         return ap_b0
 
     def test_topup(self):
-        if os.getenv("GITHUB_ACTION") is not None:
-            return
+        print('test_topup:')
+        # if os.getenv("GITHUB_ACTION") is not None:
+        #     return
         pa = Series(os.path.join('data', 'DTI_6dir.zip?DTI_6dir_PA'), dtype=float)
         ap = Series(os.path.join('data', 'DTI_6dir.zip?DTI_6dir_AP'),
                     'b,bvector', dtype=float, accept_duplicate_tag=True)
@@ -66,6 +68,7 @@ class TestFSLDWI(unittest.TestCase):
             self.assertEqual(ap_corrected.shape, ap.shape)
 
     def test_bet(self):
+        print('test_bet:')
         if os.getenv("GITHUB_ACTION") is not None:
             return
         dwi = Series(os.path.join('data', 'DTI_6dir.zip?DTI_6dir_PA'), dtype=float)
@@ -76,6 +79,7 @@ class TestFSLDWI(unittest.TestCase):
         np.testing.assert_array_equal(mask.spacing, dwi.spacing)
 
     def test_eddy(self):
+        print('test_eddy:')
         if os.getenv("GITHUB_ACTION") is not None:
             return
         pa = Series(os.path.join('data', 'DTI_6dir.zip?DTI_6dir_PA'), dtype=float)
